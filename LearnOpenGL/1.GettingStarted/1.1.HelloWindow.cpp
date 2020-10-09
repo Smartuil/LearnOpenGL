85fc27d5ad9973281bb548be8075c21208c90eb7
Smartuil <563412673@qq.com>
2020-10-09 15:59:46 +0800

    1.GettingStarted

--- LearnOpenGL/1.GettingStarted/1.1.HelloWindow.cpp
+++ LearnOpenGL/1.GettingStarted/1.1.HelloWindow.cpp
@@ -67,7 +67,7 @@ int main()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
 
@@ -78,4 +78,5 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+	std::cout << width << height << std::endl;
 }
